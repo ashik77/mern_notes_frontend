f692cdc52906a7a5a45432f982688dcf02c5f19a
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-14 15:37:25 +0200

    redux toolkit, users and notes shown in table

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
