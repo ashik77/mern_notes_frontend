24d633d87689e976d3ed0601d235790e64a84064
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-15 09:17:26 +0200

    notes crud

--- src/features/notes/NewNote.js
+++ src/features/notes/NewNote.js
@@ -1,7 +1,12 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { selectAllUsers } from "../users/usersApiSlice";
+import NewNoteForm from "./NewNoteForm";
 
 const NewNote = () => {
-  return <div>NewNote</div>;
-};
+  const users = useSelector(selectAllUsers);
 
+  const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>;
+
+  return content;
+};
 export default NewNote;
