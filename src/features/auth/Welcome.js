afbd462a8bdf206ad11c528fcea33adb53bbed7e
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-15 15:49:03 +0200

    easy navigation and welcome page

--- src/features/auth/Welcome.js
+++ src/features/auth/Welcome.js
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const Welcome = () => {
+  const { username, isManager, isAdmin } = useAuth();
+
   const date = new Date();
   const today = new Intl.DateTimeFormat("en-US", {
     dateStyle: "full",
@@ -11,7 +14,7 @@ const Welcome = () => {
     <section className="welcome">
       <p>{today}</p>
 
-      <h1>Welcome!</h1>
+      <h1>Welcome {username}!</h1>
 
       <p>
         <Link to="/dash/notes">View techNotes</Link>
@@ -21,13 +24,17 @@ const Welcome = () => {
         <Link to="/dash/notes/new">Add New techNote</Link>
       </p>
 
-      <p>
-        <Link to="/dash/users">View User Settings</Link>
-      </p>
+      {(isManager || isAdmin) && (
+        <p>
+          <Link to="/dash/users">View User Settings</Link>
+        </p>
+      )}
 
-      <p>
-        <Link to="/dash/users/new">Add New User</Link>
-      </p>
+      {(isManager || isAdmin) && (
+        <p>
+          <Link to="/dash/users/new">Add New User</Link>
+        </p>
+      )}
     </section>
   );
 
