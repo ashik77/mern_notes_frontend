37fd52887300d214c03f2830799de6ec8b456b11
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-15 15:43:06 +0200

    footer showing username and status

--- src/components/DashFooter.js
+++ src/components/DashFooter.js
@@ -1,8 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const DashFooter = () => {
+  const { username, status } = useAuth();
+
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
@@ -24,11 +27,10 @@ const DashFooter = () => {
   const content = (
     <footer className="dash-footer">
       {goHomeButton}
-      <p>Current User:</p>
-      <p>Status:</p>
+      <p>Current User: {username}</p>
+      <p>Status: {status}</p>
     </footer>
   );
   return content;
 };
-
 export default DashFooter;
