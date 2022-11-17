975a82f31c14674c47e32504f768f775c645cf51
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-17 12:41:00 +0200

    backend url updated

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://notes-api.onrender.com",
+  baseUrl: "https://notes-api-hpcx.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
